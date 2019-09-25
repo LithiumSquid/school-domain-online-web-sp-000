c809c6434d2325994bb20f3c585a33f58e3f1321
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-09-25 23:23:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,20 +1,19 @@
 class School
 attr_accessor :name, :roster
-attr
 
 def initialize(name)
   @name = name
   @roster = {}
   end 
 
-def add_student(name, grade)
-  roster[grade] = []
-  roster[grade] << name
+def add_student(name, year)
+  roster[year] = []
+  roster[year] << name
 end
 
-def grade(grade)
+def grade(year)
   roster.find do |x,y|
-    if x == grade
+    if x == year
       return y
     end
   end
